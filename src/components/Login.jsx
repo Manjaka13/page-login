731de08e75a8b74cee9f264c3f55b5d5bbf46948
Manjaka13 <manjaka.rajaonson@gmail.com>
2021-09-17 16:51:56 +0300

    adjust details for form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,12 +2,63 @@
     Login primary component
 */
 
-import React from "react";
+import { useState, useRef } from "react";
 import logo from "../images/logo-avenir-green.svg";
 import illustration from "../images/illustration.png";
-import togglePassword from "../images/togglePassword.png";
+import togglePasswordImage from "../images/togglePassword.png";
 
 export default function Login() {
+	// Store error states
+	const [errorEmail, setErrorEmail] = useState("");
+	const [errorPassword, setErrorPassword] = useState("");
+
+	//Reference the inputs
+	const refEmail = useRef("");
+	const refPassword = useRef("");
+
+	// Regex for testing if email is valid
+	const validateEmail = (str) => {
+		const regex =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return regex.test(String(str).toLowerCase());
+	};
+
+	// Tests the login form for any errors
+	const testSubmit = () => {
+		let valid = true;
+		const email = refEmail.current.value;
+		const password = refPassword.current.value;
+		if (!validateEmail(email)) {
+			setErrorEmail("L'adresse mail est invalide.");
+			valid = false;
+		}
+		if (password.length < 3) {
+			setErrorPassword("Le mot de passe est trop court.");
+			valid = false;
+		}
+		return valid;
+	};
+
+	// Handles login form submit
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (testSubmit()) {
+			alert("Connexion... <>");
+		}
+	};
+
+	// When input is focused
+	const focusInput = (field) => {
+		if (field === "email") setErrorEmail("");
+		else if (field === "password") setErrorPassword("");
+	};
+
+	// Toggles password visibility
+	const togglePassword = () => {
+		const { type } = refPassword.current;
+		refPassword.current.type = type === "password" ? "text" : "password";
+	};
+
 	return (
 		<div className="login">
 			{/* Top nav */}
@@ -32,33 +83,53 @@ export default function Login() {
 					<p className="label">
 						Proposé par <span className="name">Avenir Green</span>
 					</p>
-					<form className="form">
-						<div className="inputbox">
-							<input className="input" type="email" placeholder="E-MAIL" required />
-							<p className="error error--active">Cet e-mail est invalide.</p>
-						</div>
+
+					{/* The form */}
+					<form className="form" onSubmit={handleSubmit}>
+						{/* Email */}
 						<div className="inputbox">
 							<input
+								ref={refEmail}
+								onFocus={() => focusInput("email")}
+								className="input"
+								type="email"
+								placeholder="E-MAIL"
+								required
+							/>
+							<p className="error">{errorEmail}</p>
+						</div>
+
+						{/* Password */}
+						<div className="inputbox">
+							<input
+								ref={refPassword}
+								onFocus={() => focusInput("password")}
 								className="input"
 								type="password"
 								placeholder="MOT DE PASSE"
 								required
 							/>
-							<p className="error error--active">Cet mot de passe est invalide.</p>
-							<a className="toggle-password" href="#0">
+							<p className="error">{errorPassword}</p>
+							<a className="toggle-password" href="#0" onClick={togglePassword}>
 								<img
 									className="toggle-image"
-									src={togglePassword}
+									src={togglePasswordImage}
 									alt="Toggle password"
 								/>
 							</a>
 						</div>
+
+						{/* Forgot password */}
 						<a className="forgot-password" href="#0">
 							Oups, mot de passe oublié ?
 						</a>
+
+						{/* Submit button */}
 						<button className="button" type="submit">
 							CONNEXION
 						</button>
+
+						{/* Found an issue */}
 						<p className="issue">
 							Vous rencontrez un problème ?{" "}
 							<a className="contact" href="#0">
